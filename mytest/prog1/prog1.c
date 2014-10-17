addf3b0af35fd88f03e3ed451322e8e213067637
Sahil Parmar <sahilpparmar@gmail.com>
2014-10-17 02:06:30 -0400

    Supporting non-8 byte loads with sign extension

--- mytest/prog1/prog1.c
+++ mytest/prog1/prog1.c
@@ -1,10 +1,11 @@
 #include "syscall.h"
 
-int h = 6;
+char h1 = 'E', h2;
+int h3 = 1;
 char hello[100] = "Hello World\n";
 long main() {
-    h = 'E';
-    hello[1] = h;
+    h2 = h3 + h1;
+    hello[1] = h2;
     __syscall3(__NR_write, 1, (long long) hello, 13);
     return 1;
 }
