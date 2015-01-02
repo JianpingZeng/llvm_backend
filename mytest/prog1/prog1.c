b08000bb3790c37d0497320c686e527576528577
Sahil Parmar <sahilpparmar@gmail.com>
2015-01-02 00:47:34 -0500

    Adding Project Report with major code changes and TODOs. Also adding entire SpecInt tests

--- mytest/prog1/prog1.c
+++ mytest/prog1/prog1.c
@@ -1,11 +1,7 @@
 #include "syscall.h"
 
-char h1 = 'E', h2;
-int h3 = 1;
 char hello[100] = "Hello World\n";
 int main() {
-    h2 = h3 + h1;
-    hello[1] = h2;
     __syscall3(__NR_write, 1, (long long) hello, 13);
-    return 1;
+    return 0;
 }
